6f7a60916356edb8fb49c87204cafa46376f4500
Gabriela Mauricio <gabismauricio@gmail.com>
2019-06-25 17:54:48 -0300

    Initial Commit

---
+++ testing_swipe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testing_swipe
+//
+//  Created by Gabriela Schirmer | MundiPagg on 25/06/19.
+//  Copyright © 2019 Gabriela Schirmer | MundiPagg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ testing_swipeTests/testing_swipeTests.swift
@@ -0,0 +1,34 @@
+//
+//  testing_swipeTests.swift
+//  testing_swipeTests
+//
+//  Created by Gabriela Schirmer | MundiPagg on 25/06/19.
+//  Copyright © 2019 Gabriela Schirmer | MundiPagg. All rights reserved.
+//
+
+import XCTest
+@testable import testing_swipe
+
+class testing_swipeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
